3177c0c25f2878baf765adfa72cf138a17a8ec39
gowthamreilly <dev@gowthamreilly.com>
2021-03-13 15:05:13 +0530

    Completed challenge 4

--- 09-Data-Structures-Operators/challenges.js
+++ 09-Data-Structures-Operators/challenges.js
@@ -361,3 +361,92 @@ whether it's in the first half or second half (after 45 min) of the game, like t
 for (const [key, value] of gameEvents) {
   console.log(`[${key < 46 ? "FIRST" : "SECOND"} HALF] ${key}: ${value}`);
 }
+
+// !Challenge 4
+
+console.log(
+  "---------------------------------- Challenge 4 -----------------------------------"
+);
+
+/* 
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+underscore_case
+  first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+underscoreCase    ✅
+firstName         ✅✅
+someVariable      ✅✅✅
+calculateAge      ✅✅✅✅
+delayedDeparture  ✅✅✅✅✅
+
+Hints:
+1. Remember which character defines a new line in the textarea 😉
+2. The solution only needs to work for a variable made out of 2 words, like a_b
+3. Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+4. This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+// !My solution
+const textBox = document.querySelector("textarea");
+const btnCheck = document.querySelector("button");
+
+const convertToCamelCase = function (names) {
+  const splittedLines = names.split("\n");
+
+  const arrClean = [];
+
+  for (const str of splittedLines) {
+    arrClean.push(str.trim().toLowerCase().split("_"));
+  }
+
+  const arrFinal = [];
+
+  for (const [first, last] of arrClean) {
+    arrFinal.push(first + last.replace(last[0], last[0].toUpperCase()));
+  }
+
+  for (const [pos, el] of arrFinal.entries()) {
+    console.log(`${el.padEnd(20, " ")} ${"✅".repeat(pos + 1)}`);
+  }
+};
+
+btnCheck.addEventListener("click", function () {
+  convertToCamelCase(textBox.value);
+});
+
+// !Jonas solution
+
+// document.querySelector("button").addEventListener("click", function () {
+//   const text = document.querySelector("textarea").value;
+
+//   const rows = text.split("\n");
+
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.trim().toLowerCase().split("_");
+
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+
+//     console.log(`${output.padEnd(20, " ")}${`✅`.repeat(i + 1)}`);
+//   }
+// });
