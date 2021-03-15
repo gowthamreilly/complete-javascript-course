5a3fd469b23fcc232557826f1c2c1a082990eec9
gowthamreilly <dev@gowthamreilly.com>
2021-03-15 11:02:19 +0530

    Completed 2 challenges in function section

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+const greet = (greeting) =>
+  function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+
+const sayHey = greet("Hey");
+
+sayHey("Gowtham");
+
+const addTAX = (rate) => (value) => value + value * rate;
+
+const addVAT = addTAX(0.23);
+
+console.log(addVAT(100));
